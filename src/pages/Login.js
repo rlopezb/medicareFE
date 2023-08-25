e1800d9e0bbb4df855d10b8e9c303baf9ca92866
rlopezb <rlopezb@gmail.com>
2023-08-25 11:58:41 +0200

    Folder structure

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -73,7 +73,7 @@ function Login() {
                 <Link to="/register" className="text-primary fw-bold">Sign up</Link>
               </p>
               <p>
-                V0.0.5
+                V0.0.6
               </p>
             </div>
           </Card.Body>
